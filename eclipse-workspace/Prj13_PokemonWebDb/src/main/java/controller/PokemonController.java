41746fc99c21988a0c77d629903c5fe2951ee7c5
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2025-04-23 16:56:46 +0200

    agg

--- eclipse-workspace/Prj13_PokemonWebDb/src/main/java/controller/PokemonController.java
+++ eclipse-workspace/Prj13_PokemonWebDb/src/main/java/controller/PokemonController.java
@@ -1,24 +1,37 @@
 package controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.json.JSONArray;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import model.Pokemon;
 import service.PokemonService;
 
-public class PokemonController {
+@WebServlet("/api")
+public class PokemonController extends HttpServlet{
 
 	private PokemonService service = new PokemonService();
-	
-	public static void main(String[] args) {
 
-		PokemonController ctrl = new PokemonController();
-		
-		List<Pokemon> lista =  ctrl.service.getAllPokemon();
-		
-		for (Pokemon pokemon : lista) {
-			System.out.println(pokemon);
-		}
+	@Override
+	protected void doGet(HttpServletRequest richiesta, HttpServletResponse risposta) throws ServletException, IOException {
 
+		risposta.setContentType("application/json");//Tipo MIME
+
+		JSONArray array = new JSONArray(service.getAllPokemon());
+		
+		risposta.getWriter().print(array.toString());
+		
+		
+		
 	}
-
+	
+	
+	
+	
 }
